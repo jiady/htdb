340ce356cc3b5295bf446ef95047d937223eb20e
jiadongyu <dongyuj.vip@gmail.com>
2016-07-27 23:40:12 +0800

    don't want these files anymore, you should build it or generate it in your local machine

---
+++ crawler/crawler/config_private.py
@@ -0,0 +1,4 @@
+mail_domain = "sandbox0f2b25cd71e14ea48f25b4504c97c87f.mailgun.org"
+mail_api_key = "key-1ffadfafc4fd889c06aa88fe7cd6d87c"
+mail_http_url = "https://api.mailgun.net/v3/" + mail_domain + "/messages"
+mail_to_address = ["893392749@qq.com"]
